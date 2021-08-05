1ba9a2630b0af4eb82441b84ac3607dfdc6480df
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-04 23:37:00 -0500

    feat:  my first component Card

---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="card">
+        <h3>This is a Card</h3>
+        <div>{{ msg }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+    props: {
+        msg: String,
+    }
+})
+
+export default class Card extends Vue {
+    msg!: string;
+}
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        padding: 20px;
+        margin: 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        min-width: 0;
+        word-wrap: break-word;
+        width: 20%;
+        cursor: pointer;
+    }
+    .card:hover {
+        background-color: #f6f6f6;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome to My first Vue app!" />
+    <div class="container">
+    <Card msg="First Card"/>
+    <Card msg="Second Card"/>
+    <Card msg="Third Card"/>
+    <Card msg="Fourth Card"/>
+    <Card msg="Fifth Card"/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Card from "@/components/Card.vue";
 
 @Options({
   components: {
     HelloWorld,
+    Card,
   },
 })
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+}
+</style>
