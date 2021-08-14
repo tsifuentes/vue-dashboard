7eecf19892b7f5b7dc436f43b7f12e9543170064
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-14 00:06:35 -0500

    feat: adding action on button to catch values

--- src/views/Patient.vue
+++ src/views/Patient.vue
@@ -1,40 +1,40 @@
 <template>
     <h1>New patient</h1>
     <div class="container">
-    <div class="form">
-        <label>
-            Name:
-            <input v-model="name"/>
-        </label>
-        <label>
-            Last name:
-            <input v-model="lastName"/>
-        </label>
-        <label>
-            Phone:
-            <input v-model="phone" max-length="9" min-length="9"/>
-        </label>
-        <label>
-            Birthdate:
-            <input v-model="birthdate" type="date"/>
-        </label>
-        <label>
-            Address:
-            <input v-model="address"/>
-        </label>
-        <label>
-            Record:
-            <input v-model="record"/>
-        </label>
-    </div>
-    <div class="card">
-        <h3>Card:</h3>
-        <div>{{name + ' ' + lastName}}</div>
-        <div>{{phone}}</div>
-        <div>{{birthdate}}</div>
-        <div>{{address}}</div>
-        <div>{{record}}</div>
-    </div>
+        <div class="form">
+            <label>
+                Name:
+                <input v-model="name"/>
+            </label>
+            <label>
+                Last name:
+                <input v-model="lastName"/>
+            </label>
+            <label>
+                Phone:
+                <input v-model="phone" max-length="9" min-length="9"/>
+            </label>
+            <label>
+                Birthdate:
+                <input v-model="birthdate" type="date"/>
+            </label>
+            <label>
+                Address:
+                <input v-model="address"/>
+            </label>
+            <label>
+                Record:
+                <input v-model="record"/>
+            </label>
+        </div>
+        <div class="card">
+            <h3>Card:</h3>
+            <div>{{fullName}}</div>
+            <div>{{phone}}</div>
+            <div>{{birthdate}}</div>
+            <div>{{address}}</div>
+            <div>{{record}}</div>
+        </div>
     </div>
     <button v-on:click="savePatient">Save</button>
 </template>
@@ -49,7 +49,11 @@ export default class Patient extends Vue {
     birthdate = "";
     address = "";
     record = "";
+    fullName="";
 
+    savePatient = () => {
+        this.fullName = `${this.name} ${this.lastName}`;
+    };
 }
 </script>
 
@@ -88,7 +92,7 @@ export default class Patient extends Vue {
     button {
         border-radius: 10px;
         background-color: blue;
-        color: #FFF;
+        color: #fff;
         font-weight: bold;
         padding: 10px 20px;
         border: 1px solid blue;
